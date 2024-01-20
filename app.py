473768ed1ecfa68608cd6204147619cdb1948e52
VIvidh Mahajan <vividhmahajan@gmail.com>
2024-01-20 18:08:37 -0500

    Updated the new api key

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+import json
 import requests
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 
 headers = {
   'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  'x-rapidapi-key': "4aba1d5a7f7a460197e6b460331e799e",
+  'x-rapidapi-key': "815abe79damsh508dd88101127e1p140d1ajsnf54e52aadad1",
   }
 
 random_joke = "food/jokes/random"
@@ -16,29 +17,8 @@ randomFind = "recipes/random"
 
 @app.route('/')
 def search_page():
-  joke_response = "Hello" #str(requests.request("GET", url + random_joke, headers=headers).json()['text'])""
+  joke_response = "Hllo" #str(requests.request("GET", url + random_joke, headers=headers).json()['text'])""
   return render_template('search.html', joke=joke_response)
-
-@app.route('/recipe')
-def get_recipe():
-  recipe_id = request.args['id']
-  recipe_info_endpoint = "recipes/{0}/information".format(recipe_id)
-  ingedientsWidget = "recipes/{0}/ingredientWidget".format(recipe_id)
-  equipmentWidget = "recipes/{0}/equipmentWidget".format(recipe_id)
-
-  recipe_info = requests.request("GET", url + recipe_info_endpoint, headers=headers).json()
-    
-  recipe_headers = {
-      'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      'x-rapidapi-key': "4aba1d5a7f7a460197e6b460331e799e",
-      'accept': "text/html"
-  }
-  querystring = {"defaultCss":"true", "showBacklink":"false"}
-
-  recipe_info['inregdientsWidget'] = requests.request("GET", url + ingedientsWidget, headers=recipe_headers, params=querystring).text
-  recipe_info['equipmentWidget'] = requests.request("GET", url + equipmentWidget, headers=recipe_headers, params=querystring).text
-    
-  return render_template('recipe.html', recipe=recipe_info)
 
 @app.route('/recipes')
 def get_recipes():
@@ -53,6 +33,28 @@ def get_recipes():
       response = requests.request("GET", url + randomFind, headers=headers, params=querystring).json()
       print(response)
       return render_template('recipes.html', recipes=response['recipes'])
+  
+@app.route('/recipe')
+def get_recipe():
+  recipe_id = request.args['id']
+  recipe_info_endpoint = "recipes/{0}/information".format(recipe_id)
+  ingedientsWidget = "recipes/{0}/ingredientWidget".format(recipe_id)
+  equipmentWidget = "recipes/{0}/equipmentWidget".format(recipe_id)
+
+  recipe_info = requests.request("GET", url + recipe_info_endpoint, headers=headers).json()
+    
+  recipe_headers = {
+      'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      'x-rapidapi-key': "815abe79damsh508dd88101127e1p140d1ajsnf54e52aadad1",
+      'accept': "text/html"
+  }
+  querystring = {"defaultCss":"true", "showBacklink":"false"}
+
+  recipe_info['inregdientsWidget'] = requests.request("GET", url + ingedientsWidget, headers=recipe_headers, params=querystring).text
+  recipe_info['equipmentWidget'] = requests.request("GET", url + equipmentWidget, headers=recipe_headers, params=querystring).text
+    
+  return render_template('recipe.html', recipe=recipe_info)
+
 
 if __name__ == '__main__':
   app.run()
